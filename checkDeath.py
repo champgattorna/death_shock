c71cac718f75175ac924328f3919f69a39a25757
Christian Hamp-Gattorna <67287207+champgattorna@users.noreply.github.com>
2020-06-23 16:10:07 -0400

    Add files via upload
    Added while loop to test for when screenshot code is added

--- checkDeath.py
+++ checkDeath.py
@@ -2,21 +2,31 @@
 # Begin imports
 
 import pyautogui
+import PIL
 from riotwatcher import LolWatcher, ApiError
 
 # Begin Riot API stuff
 lol_watcher = LolWatcher('RGAPI-684824e2-bcf4-4230-bff0-365e1d3e4c01')
 my_region = 'na1'
 # C0RONA VARUS
-me = 'ke5FOarkmWE1EY2AhND8KF6I2KsN7Xq7K1v5lxjphkKKwbc'
+me = 'YYfvFX2raeodh6s2jeOhyf-72NEws38j53OXLEVFQR-TlXs'
 
 # Check to see if summoner is in an active match
-active_gameId = None
 try:
     in_game = lol_watcher.spectator.by_summoner(my_region, me)
 # If summoner is not in a game, one of the response codes will display
 except ApiError as err:
     print('This summoner is not in an active game')
+    active_gameId = None
 else:
     active_gameId = in_game['gameId']
-    print(active_gameId)
+    while active_gameId != None:
+        try:
+            in_game = lol_watcher.spectator.by_summoner(my_region, me)
+        except ApiError as err:
+            break
+        else:
+            print('Summoner is in game! POGFISH')
+
+
+
