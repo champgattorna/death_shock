f55901ddf0a2b44d285ba00f4f76c6f51166bfe0
Christian Hamp-Gattorna <67287207+champgattorna@users.noreply.github.com>
2020-06-23 21:35:14 -0400

    Add files via upload
    Added a function to grab a screenshot and check if the average RGB is in range of being in greyscale. Also added a simple if-else to print whether or not to shock based on the boolean returned by the function

--- checkDeath.py
+++ checkDeath.py
@@ -2,13 +2,31 @@
 # Begin imports
 
 import time
-import pyautogui
-import PIL
+from PIL import Image, ImageGrab
 from riotwatcher import LolWatcher, ApiError
 
 # Riot API Setup
 lol_watcher = LolWatcher('RGAPI-19397e0d-11ce-4d0d-8056-cd0d3f61c613')
 my_region = 'na1'
+
+# Function to grab a screenshot, and check if the average RGB is in range of greyscale
+def greyscaleCheck():
+    screenshot = ImageGrab.grab(bbox=(500, 500, 900, 750))
+
+    numpixels = screenshot.size[0] * screenshot.size[1]
+    colors = screenshot.getcolors(numpixels)
+    sumRGB = [(x[0] * x[1][0], x[0] * x[1][1], x[0] * x[1][2]) for x in colors]
+    avg = tuple([sum(x) / numpixels for x in zip(*sumRGB)])
+
+    # Greyscale range based on tested values
+    grey_upper = (57, 57, 56)
+    grey_lower = (0, 0, 0)
+
+    if avg >= grey_lower and avg <= grey_upper:
+        return True
+
+    else:
+        return False
 
 # Prompts user for their summoner name
 summoner = input('Enter a valid summoner name: ')
@@ -34,8 +52,9 @@ else:
             break
         else:
             time.sleep(2) # Delay to not hit request ceiling for Riot API
-            print('Summoner is in game! POGFISH')
-
-
+            if greyscaleCheck():
+                print('Shock that bitch!')
+            else:
+                print('The player is alive!')
 
 
