38ab89a6a249c247d8c158a6424da66041656a3b
Christian Hamp-Gattorna <67287207+champgattorna@users.noreply.github.com>
2020-06-23 20:18:39 -0400

    Add files via upload
    Automated getting id by having user input summoner name so id doesn't have to be hard-coded in. Also added sleep in while loop to not over request from Riot API

--- checkDeath.py
+++ checkDeath.py
@@ -1,15 +1,19 @@
 # File to check if the player has died during a game of League of Legends
 # Begin imports
 
+import time
 import pyautogui
 import PIL
 from riotwatcher import LolWatcher, ApiError
 
-# Begin Riot API stuff
-lol_watcher = LolWatcher('RGAPI-684824e2-bcf4-4230-bff0-365e1d3e4c01')
+# Riot API Setup
+lol_watcher = LolWatcher('RGAPI-19397e0d-11ce-4d0d-8056-cd0d3f61c613')
 my_region = 'na1'
-# C0RONA VARUS
-me = 'YYfvFX2raeodh6s2jeOhyf-72NEws38j53OXLEVFQR-TlXs'
+
+# Prompts user for their summoner name
+summoner = input('Enter a valid summoner name: ')
+get_id = lol_watcher.summoner.by_name(my_region, summoner)
+me = (get_id['id'])
 
 # Check to see if summoner is in an active match
 try:
@@ -20,13 +24,18 @@ except ApiError as err:
     active_gameId = None
 else:
     active_gameId = in_game['gameId']
+
+    # Begins loop for screenshots and math
     while active_gameId != None:
         try:
+            # Second try is needed in case the summoner is no longer in an active match
             in_game = lol_watcher.spectator.by_summoner(my_region, me)
         except ApiError as err:
             break
         else:
+            time.sleep(2) # Delay to not hit request ceiling for Riot API
             print('Summoner is in game! POGFISH')
+
 
 
 
